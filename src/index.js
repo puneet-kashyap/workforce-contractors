c9604feb8ec1780b4fdc4c2fc930860b471a0880
Puneet Kashyap <pkashyaps@gmail.com>
2019-03-22 15:09:17 -0400

    Added redux and redux form modules

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'Redux';
+
 import './index.css';
 import App from './App';
+import combinedReducers from './reducers'
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={createStore(combinedReducers)}>
+    <App />, document.getElementById('root')
+  </Provider>
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
