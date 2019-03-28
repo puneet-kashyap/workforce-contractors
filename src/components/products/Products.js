b1d37c7cc2aa707892db6ebed253794fe48bb61e
Puneet Kashyap <pkashyaps@gmail.com>
2019-03-28 14:08:02 -0400

    Product card responsiveness added

--- src/components/products/Products.js
+++ src/components/products/Products.js
@@ -8,37 +8,37 @@ import { productList } from './productList';
 
 class Products extends React.Component {
 
-  cardStyles = {margin: '5px', maxWidth: '400px'}
-
   render() {
     return (
       <>
-      <h1>Tool Rentals</h1>
-      <div className="products">
-        {productList.map(product => {
-          return (
-            <Card key={product.id} raised style={this.cardStyles}>
-              <CardContent>
-                <h2>{product.title}</h2>
-                <img
-                  className="tool-img"
-                  src={product.img}
-                  alt={product.title}
-                />
-              </CardContent>
-              <CardActions>
-                <Button
-                  variant="contained"
-                  color="primary"
-                  style={{ margin: 'auto' }}
-                >
-                  Rent It
-                </Button>
-              </CardActions>
-            </Card>
-          );
-        })}
-      </div>
+        <h1>Tool Rentals</h1>
+        <div className="products">
+          {productList.map(product => {
+            return (
+              <div className="card">
+                <Card key={product.id} raised>
+                  <CardContent>
+                    <h2>{product.title}</h2>
+                    <img
+                      className="tool-img"
+                      src={product.img}
+                      alt={product.title}
+                    />
+                  </CardContent>
+                  <CardActions>
+                    <Button
+                      variant="contained"
+                      color="primary"
+                      style={{ margin: 'auto' }}
+                    >
+                      Rent It
+                    </Button>
+                  </CardActions>
+                </Card>
+              </div>
+            );
+          })}
+        </div>
       </>
     );
   }
