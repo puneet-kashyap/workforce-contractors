07b58aee1755ed21987219a78039a51a4287b74c
Puneet Kashyap <pkashyaps@gmail.com>
2019-04-02 13:31:59 -0400

    Inquiry form validation added

--- src/components/inquiry/InquiryForm.js
+++ src/components/inquiry/InquiryForm.js
@@ -5,21 +5,48 @@ import Card from '@material-ui/core/Card';
 import { Button } from '@material-ui/core';
 import { reduxForm } from 'redux-form';
 
+const validate = values => {
+  const errors = {};
+  const requiredFields = [
+    'name',
+    'phone',
+    'comments'
+  ]
+
+  requiredFields.forEach(field => {
+    if (!values[field]) {
+      errors[field] = 'Required'
+    }
+  });
+
+  if (
+    values.email &&
+    !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)
+  ) {
+    errors.email = 'Invalid email address'
+  }
+
+  if(values.phone && !/^(0|[+]{0,1}[1-9][0-9]{9,12})$/i.test(values.phone)){
+    errors.phone = "Please enter your 10 digit phone number."
+  }
+  return errors
+}
+
 const renderTextField = ({
   input,
   label,
-  meta: { touched, error },
+  meta: { touched, error, invalid },
   ...custom
 }) => (
   <TextField
     label={label}
     variant="outlined"
-    errorText={touched && error}
+    error={touched && invalid}
+    helperText={touched && error}
     {...input}
     {...custom}
     className="text-field"
     fullWidth
-    required
     style={{marginBottom: "10px"}}
   />
 );
@@ -71,5 +98,6 @@ class InquiryForm extends React.Component {
 }
 
 export default reduxForm({
-  form: 'inquiryForm'
+  form: 'inquiryForm',
+  validate
 })(InquiryForm);
