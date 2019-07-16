a95cc29aa70be741b9dd3748e31c7ff273ed9902
Puneet Kashyap <pkashyaps@gmail.com>
2019-07-16 11:49:40 -0400

    Product reducer and thunk added

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import thunk from 'redux-thunk'
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 
 import './index.css';
 import App from './App';
 import rootReducer from './redux/reducers'
 import * as serviceWorker from './serviceWorker';
 
-const store = createStore(rootReducer)
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 ReactDOM.render(
   <Provider store={store}>
--- src/redux/actions/products.action.js
+++ src/redux/actions/products.action.js
@@ -2,16 +2,16 @@ import productApi from '../../apis/productsApi';
 
 export const getAllProducts = async dispatch => {
   const allProducts = await productApi.get('/products');
-  return {
+  dispatch({
     type: GET_ALL_PRODUCTS,
     payload: allProducts
-  }
+  });
 }
 
 export const getProduct = id => async dispatch => {
   const product = await productApi.get(`/products/${id}`);
-  return {
-    type: GET_ALL_PRODUCTS,
+  dispatch({
+    type: GET_PRODUCT,
     payload: product
-  }
+  });
 }
